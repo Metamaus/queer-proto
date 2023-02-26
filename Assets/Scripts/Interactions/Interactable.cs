d475100f5de4988e9485151ef2ca3174afc92bcb
Metamaus <leonard.rizzo71@hotmail.fr>
2023-02-26 17:27:29 -0500

    feat: interact

---
+++ Assets/Scripts/Interactions/Interactable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interactions
+{
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField] private Transform _interactionLocation;
+        
+        private bool _hasInteracted;
+        private PlayerController _interactingEntity;
+        private Action _onInteract;
+
+        public Transform SetFocus(PlayerController player, Action onInteract = null)
+        {
+            if (onInteract != null)
+            {
+                _onInteract += onInteract;
+            }
+            _interactingEntity = player;
+            
+            return _interactionLocation;
+        }
+
+        private void UnFocus()
+        {
+            _interactingEntity = null;
+        }
+
+        public void Interact()
+        {
+            print("[Interactable] Interact");
+            _onInteract.Invoke();
+            _onInteract = null; // is this okay ?
+            UnFocus();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.GetComponentInParent<PlayerController>() == _interactingEntity) // todo(interactions): identify entity better
+                Interact();
+        }
+    }
+}
--- Assets/Scripts/PlayerBehavior/PlayerController.cs
+++ Assets/Scripts/PlayerBehavior/PlayerController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Interactions;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -11,15 +14,39 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
-            print("[PlayerController] Click");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            // Todo (navigation): ignore obstacles ?
+            // Todo (navigation): ignore obstacles when clicking ?
             if (Physics.Raycast(ray, out hit))
             {
-                _navMeshAgent.SetDestination(hit.point);
+                if (hit.collider.CompareTag("Walkable"))
+                {
+                    _navMeshAgent.isStopped = false; // todo(movement): block player movement better
+                    _navMeshAgent.destination = hit.point;
+                }
+                else if (hit.collider.CompareTag("Interactable"))
+                {
+                    var interactable = hit.transform.gameObject.GetComponentInParent<Interactable>();
+                    if (interactable)
+                    {
+                        Vector3 targetPosition = interactable.SetFocus(this, OnReachInteractable).position;
+                        if (Vector3.Distance(targetPosition, transform.position) < _navMeshAgent.stoppingDistance)
+                        {
+                            interactable.Interact();
+                        }
+                        else
+                        {
+                            _navMeshAgent.destination = targetPosition;
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private void OnReachInteractable()
+    {
+        // todo(interaction) : rotate toward the target
     }
 }

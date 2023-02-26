42546763efdfe77ba56b7cd67f9a9e3ff27256f8
Metamaus <leonard.rizzo71@hotmail.fr>
2023-02-26 12:50:07 -0500

    feat: player navigation on click

---
+++ Assets/Scripts/PlayerBehavior/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent _navMeshAgent;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("[PlayerController] Click");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // Todo (navigation): ignore obstacles ?
+            if (Physics.Raycast(ray, out hit))
+            {
+                _navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
